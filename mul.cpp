ebd3b649b18418ebaaeb0c8e7efb79f40cc5ea28
Vishal Sharma <sharma.vishal449@gmail.com>
2022-08-31 22:06:13 +0530

    added multiplication file

---
+++ mul.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+
+    //todo: add multiplication
+    int a = 10;
+    int b = 20;
+    int c = a*b;
+    cout<<"result is : "<<c;
+
+    return 0;
+}
